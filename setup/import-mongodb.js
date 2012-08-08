5da2c61a7e65a27a531ba7212a538080101b487d
Austin Wright <diamondmagic@users.sourceforge.net>
2012-08-07 23:31:10 -0400

    Move the MongoDB dat import logic to a library

--- setup/import-mongodb.js
+++ setup/import-mongodb.js
@@ -1,17 +1,21 @@
 #!/usr/bin/env node
 var mongodb = require('mongolian');
 var parseMongoJSON = require('./parsemongojson');
+var util=require('util');
 
 var arguments = process.argv.slice(2);
 var dbHost, dbName, dbUsername, dbPassword;
 var file = 'mongodb.json';
 for(var i=0; i<arguments.length; i++){
-	if(arguments[i]=='-?'||arguments[i]=='--help') return printHelp();
-	if(arguments[i]=='-h'||arguments[i]=='--host') dbHost=arguments[++i];
-	if(arguments[i]=='-d'||arguments[i]=='--db') dbName=arguments[++i];
-	if(arguments[i]=='-u'||arguments[i]=='--username') dbUsername=arguments[++i];
-	if(arguments[i]=='-p'||arguments[i]=='--password') dbPassword=arguments[++i];
-	if(arguments[i]=='-f'||arguments[i]=='--file') file=arguments[++i];
+	switch(arguments[i]){
+		case '-?':case '--help': return printHelp();
+		case '-h':case '--host': dbHost=arguments[++i]; continue;
+		case '-d':case '--db': dbName=arguments[++i]; continue;
+		case '-u':case '--username': dbUsername=arguments[++i]; continue;
+		case '-p':case '--password': dbPassword=arguments[++i]; continue;
+		case '-f':case '--file': file=arguments[++i]; continue;
+	}
+	throw new Error('Unknown argument '+util.inspect(arguments[i]));
 }
 
 function printHelp(){
@@ -35,7 +39,6 @@ if(dbPassword==='-'){
 
 
 function importData(){
-	var waitingQueries = 1;
 	console.error('Hostname: %s', dbHost);
 	console.error('Username: %s', dbUsername);
 	//console.error('Password: %s', dbPassword);
@@ -50,44 +53,9 @@ function importData(){
 	dbConnect.log.error = console.error;
 	var dbClient = dbConnect.db(dbName);
 	if(dbUsername) dbClient.auth(dbUsername, dbPassword);
-
-	var indexes = {};
-	if(collections['system.indexes'] instanceof Array){
-		var records = collections['system.indexes']
-		for(var i=0; i<records.length; i++){
-			var c = records[i].collection;
-			var collection = dbClient.collection(c);
-			if(!collections[c]) collections[c] = [];
-			if(!indexes[c]) indexes[c] = [];
-			indexes[c].push(records[i]);
-		}
-		delete collections['system.indexes'];
-	}
-	for(var c in collections){
-		var collection = dbClient.collection(c);
-		indexes[c] = indexes[c]||[];
-		console.log('Collection: '+c);
-		//console.log(indexes[c]);
-		for(var i=0; i<indexes[c].length; i++){
-			// (function(c,i){return function(){console.log('Imported index %s%j',c,indexes[c][i].key);}})(c,i)
-			waitingQueries++;
-			collection.ensureIndex(indexes[c][i].key, indexes[c][i].options, done);
-		}
-		//console.log(collections[c]);
-		var records = collections[c];
-		if(!records instanceof Array) throw new Error('Collection '+c+' not an Array');
-		for(var i=0; i<records.length; i++){
-			// (function(records,i){return function(){console.log('Imported row %s[%d]',c,i,records[i]);}})(records,i)
-			waitingQueries++;
-			collection.insert(records[i], done);
-		}
-	}
-	waitingQueries--;
-
-	function done(){
-		if(--waitingQueries!==0) return;
-		waitingQueries = false;
+	parseMongoJSON.importData(collections, dbClient, function(err){
 		dbConnect.close();
-	}
+		if(err){throw err;}
+	});
 }
 
--- setup/parsemongojson.js
+++ setup/parsemongojson.js
@@ -17,12 +17,44 @@ module.exports.readFileSync = function readFileSync(filename){
 	return module.exports.parseMongoJSON(content);
 }
 
-module.exports.importIndexes = function importIndexes(filename){
-	var content = fs.readFileSync(filename, 'utf8');
-	return module.exports.parseMongoJSON(content);
-}
+module.exports.importData = function importData(collections, dbClient, callback){
+	var waitingQueries = 1;
+	var indexes = {};
+	if(collections['system.indexes'] instanceof Array){
+		var records = collections['system.indexes']
+		for(var i=0; i<records.length; i++){
+			var c = records[i].collection;
+			var collection = dbClient.collection(c);
+			if(!collections[c]) collections[c] = [];
+			if(!indexes[c]) indexes[c] = [];
+			indexes[c].push(records[i]);
+		}
+		delete collections['system.indexes'];
+	}
+	for(var c in collections){
+		var collection = dbClient.collection(c);
+		indexes[c] = indexes[c]||[];
+		console.log('Collection: '+c);
+		//console.log(indexes[c]);
+		for(var i=0; i<indexes[c].length; i++){
+			// (function(c,i){return function(){console.log('Imported index %s%j',c,indexes[c][i].key);}})(c,i)
+			waitingQueries++;
+			collection.ensureIndex(indexes[c][i].key, indexes[c][i].options, done);
+		}
+		//console.log(collections[c]);
+		var records = collections[c];
+		if(!records instanceof Array) throw new Error('Collection '+c+' not an Array');
+		for(var i=0; i<records.length; i++){
+			// (function(records,i){return function(){console.log('Imported row %s[%d]',c,i,records[i]);}})(records,i)
+			waitingQueries++;
+			collection.insert(records[i], done);
+		}
+	}
+	done();
 
-module.exports.importCollection = function importCollection(filename){
-	var content = fs.readFileSync(filename, 'utf8');
-	return module.exports.parseMongoJSON(content);
+	function done(){
+		if(--waitingQueries!==0) return;
+		waitingQueries = false;
+		callback();
+	}
 }
