bf090ceeb215f0e630a4237d20cc1107627e3770
Austin Wright <diamondmagic@users.sourceforge.net>
2012-09-14 17:22:20 -0400

    Let people specify custom stylesheets/scripts to the theme

--- theme/twentyonetwelve/index.js
+++ theme/twentyonetwelve/index.js
@@ -50,11 +50,17 @@ module.exports.importTheme = function(render, router){
 	var renderDocumentFn = jade.compile(contents, {filename:templateFilename});
 	function renderDocument(db, transform, input, render, callback){
 		var outputType = db.filter({subject:transform,predicate:"http://magnode.org/view/range"}).map(function(v){return v.object;});
+		var theme = input.theme||{};
+		// Add us some presentation
 		var stylesheets = [{src:p+'codemirror.css'}, {src:p+'layout.css'}];
+		if(theme.stylesheets instanceof Array) theme.stylesheets.forEach(function(v){ stylesheets.push(v); });
+		// Add us some logic
 		var scripts = [{type:'text/javascript',src:p+'codemirror.js'}];
 		var codeMirrorModes = ['xml', 'javascript'];
 		codeMirrorModes.forEach(function(v){scripts.push({type:'text/javascript',src:p+'codemirror-mode/'+v+'/'+v+'.js'})});
 		scripts.push({type:'text/javascript',src:p+'layout.js'});
+		if(theme.scripts instanceof Array) theme.scripts.forEach(function(v){ scripts.push(v); });
+		// Add us some data
 		var locals = {input:input, stylesheets:stylesheets, scripts:scripts};
 		var result = renderDocumentFn(locals);
 		var output = {};
