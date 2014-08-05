172339f3eb3b59f501fb32aac878ea357e2fe7e3
Austin Wright <diamondmagic@users.sourceforge.net>
2014-08-05 03:05:38 -0400

    Add autocomplete widget logic

--- theme/twentyonetwelve/layout.js
+++ theme/twentyonetwelve/layout.js
@@ -1,3 +1,9 @@
+function logError(e){
+	if(typeof console=='object' && console.log){
+		console.log(e);
+	}
+}
+
 function populateForms(doc){
 	if(!doc) doc = document;
 
@@ -31,7 +37,7 @@ function populateForms(doc){
 				populateForms(clone);
 			};
 			field.appendChild(additem_li);
-		}catch(e){throw e;}
+		}catch(e){logError(e);}
 	})(es[i]);
 
 	var es = doc.getElementsByClassName('field-object-new');
@@ -70,7 +76,7 @@ function populateForms(doc){
 			};
 			field.appendChild(additem_dt);
 			field.appendChild(additem_dd);
-		}catch(e){throw e;}
+		}catch(e){logError(e);}
 	})(es[i]);
 
 	var es = doc.getElementsByClassName('field-json');
@@ -78,7 +84,7 @@ function populateForms(doc){
 		if(es[i].tagName.toLowerCase()!=='textarea') continue;
 		try{
 			var editor = CodeMirror.fromTextArea(es[i], {lineNumbers:true, indentWithTabs:true, tabSize: 4, indentUnit:4, mode:'application/json'});
-		}catch(e){}
+		}catch(e){logError(e);}
 	}
 
 	var es = doc.getElementsByClassName('field-html');
@@ -86,7 +92,7 @@ function populateForms(doc){
 		if(es[i].tagName.toLowerCase()!=='textarea') continue;
 		try {
 			var editor = CodeMirror.fromTextArea(es[i], {lineNumbers:true, lineWrapping:true, indentWithTabs:true, tabSize: 4, indentUnit:4, mode:'text/html'});
-		}catch(e){}
+		}catch(e){logError(e);}
 	}
 
 	var es = doc.getElementsByClassName('field-code');
@@ -99,7 +105,7 @@ function populateForms(doc){
 					return true;
 				}
 			});
-		}catch(e){}
+		}catch(e){logError(e);}
 	}
 
 	var es = doc.getElementsByClassName('field-switch');
@@ -119,8 +125,160 @@ function populateForms(doc){
 				}
 			}
 			hideAll(1);
-		}catch(e){throw e;}
+		}catch(e){logError(e);}
+	})(es[i]);
+
+	var es = doc.getElementsByClassName('field-autocomplete');
+	for(var i=0; i<es.length; i++) (function(field){
+		try {
+			var m = field.className.match(/(\s|^)autocomplete-endpoint<([^>\s]*)>(\s|$)/);
+			var a = new WidgetAutocomplete(field, m&&m[2]||'');
+			field.parentNode.appendChild(a.results);
+			a.onselect = function(e){
+				this.input.value = e.element && e.element.firstElementChild.textContent;
+			};
+		}catch(e){logError(e);}
 	})(es[i]);
 
 };
 document.addEventListener("DOMContentLoaded", function(){ populateForms(document); }, false);
+
+function WidgetAutocomplete(ele, endpoint){
+	this.searching = false;
+	this.currentSearch = "";
+	this.selectedIndex = -1;
+	this.timeout = null;
+	this.input = ele;
+	this.endpoint = endpoint;
+	this.results = document.createElement('div');
+	this.results.className = 'autocomplete-results';
+	this.results.style.display = 'none';
+	this.results.addEventListener('mousedown', function(e) {
+		e.preventDefault();
+	});
+	this.initElement(ele);
+	this.onselect = function(){};
+}
+WidgetAutocomplete.prototype.initElement = function initElement(e){
+	var self = this;
+	e.addEventListener('blur', function() {
+		self.resetForm();
+	});
+	e.addEventListener('keyup', function() {
+		self.runSearch();
+	});
+	e.addEventListener('keydown', function(event) {
+		switch(event.which){
+			case 8:
+			case 9:
+			case 13:
+			case 27:
+				break;
+			default:
+				self.results.style.display = 'block';
+		}
+		self.highlight(self.selectedIndex);
+		switch (event.which) {
+			case 8: // Backspace
+			case 9: // Tab
+				break;
+			case 13: // Enter
+				event.preventDefault();
+				var e = self.results.getElementsByTagName('li')[self.selectedIndex];
+				if(e) self.go(e);
+				return false;
+			case 27: // Escape
+				self.resetForm();
+				break;
+			case 38: // Up
+				event.preventDefault();
+				self.resultsNav(-1);
+				break;
+			case 40: // Down
+				event.preventDefault();
+				self.resultsNav(1);
+				break;
+		}
+	});
+};
+WidgetAutocomplete.prototype.runSearch = function runSearch() {
+	var q = this.input.value;
+	var self = this;
+	function resultsTimeout(){
+		self.searching = false;
+		self.runSearch();
+	}
+	function haveResults(e){
+		while(self.results.lastChild) self.results.removeChild(self.results.lastChild);
+		self.results.appendChild(e.getElementsByTagName('ul')[0].cloneNode(true));
+		var list = self.results.getElementsByTagName('li');
+		for(var i=0; i<list.length; i++) (function(li){
+			var alist = li.getElementsByTagName('a');
+			for(var j=0; j<alist.length; j++){
+				alist[j].onclick = function(e){
+					e.preventDefault();
+					self.go(li);
+					return false;
+				};
+			}
+		})(list[i]);
+		self.highlight();
+		self.searching = false;
+	}
+	if(q.length<2){
+		this.resetForm();
+		return false;
+	}else if(self.searching){
+		clearTimeout(self.timeout);
+		self.timeout=setTimeout(resultsTimeout, 300);
+		return;
+	}
+	self.searching=true;
+	if(q==self.currentSearch) return;
+	self.currentSearch = q;
+	self.results.textContent = '...';
+	var request = new XMLHttpRequest();
+	var requestUri = this.endpoint.replace(/{q}/, encodeURIComponent(q));
+	request.open('GET', requestUri, true);
+	request.onload = function() {
+		if (this.status>=200 && this.status<400){
+			haveResults(this.responseXML);
+		} else {
+			self.results.textContent = '[HTTP Error: '+this.status+']';
+		}
+	};
+	request.onerror = function() {
+		self.results.textContent = '[Connection Error]';
+	};
+	request.send();
+};
+WidgetAutocomplete.prototype.go = function go(e) {
+	var t = e && e.getAttribute("href");
+	if (!t) {
+		var o = this.input.value;
+		// FIXME use fully formatted endpoint
+		var t = this.endpoint + "?search=" + o;
+	}
+	this.resetForm();
+	// window.location.href = t;
+	this.onselect.call(this, {element:e});
+};
+WidgetAutocomplete.prototype.resultsNav = function resultsNav(e) {
+	// descending, so 1=="down"
+	this.selectedIndex += e;
+	this.highlight(this.selectedIndex);
+};
+WidgetAutocomplete.prototype.highlight = function highlight() {
+	var list = this.results.getElementsByTagName('li');
+	for(var i=0; i<list.length; i++){
+		list[i].className = list[i].className.replace(/(^|\b)h(\b|$)/g, '');
+	}
+	this.results.style.width = this.input.offsetWidth+'px';
+	if(this.selectedIndex > list.length) this.selectedIndex = 0;
+	if(this.selectedIndex<=-1) this.selectedIndex=-1;
+	if(list[this.selectedIndex]) list[this.selectedIndex].className += " h";
+};
+WidgetAutocomplete.prototype.resetForm = function resetForm() {
+	this.results.style.display = 'none';
+	this.selectedIndex = -1;
+};
